2009a70fcbebd2376771f77376d1941a7c660484
bnzuhair <relsalehzohair@gamil.com>
2024-10-12 01:37:33 +0300

    fix: make wezterm in a single folder

---
+++ modules/home-manager/wezterm/wezterm.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  programs.wezterm = {
+	enable = true;
+	enableZshIntegration = true;
+	enableBashIntegration = true;
+	extraConfig = ''
+	${builtins.readFile ./wezterm.lua}
+	'';
+};
+	
+
+}
