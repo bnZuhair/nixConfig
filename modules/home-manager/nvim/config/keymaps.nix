071d8810480fabb79e02f0ae77bb06e1e8cf7ab0
bnzuhair <relsalehzohair@gamil.com>
2024-10-14 05:54:45 +0300

    feat: add keymap for new lines

--- modules/home-manager/nvim/config/keymaps.nix
+++ modules/home-manager/nvim/config/keymaps.nix
@@ -1,11 +1,27 @@
 {
-  config.keymaps = [
+	config.keymaps = [
+		{
+			mode = "n";
+			action = "<cmd>nohlsearch<CR>";
+      	key = "<Esc>";
+			options = {
+      	  desc = "to Open Oil.nvim ";
+      	};
+	    }
 	{
 		mode = "n";
-      action = "<cmd>nohlsearch<CR>";
-      key = "<Esc>";
+      action = "o<Esc>k";
+      key = "<leader>o";
 		options = {
-        desc = "to Open Oil.nvim ";
+        desc = "make a new line down from normal mode and still be in it";
+      };
+    }
+	{
+		mode = "n";
+      action = "O<Esc>j";
+      key = "<leader>O";
+		options = {
+        desc = "a new line up from normal mode and still be in it";
       };
     }
 
