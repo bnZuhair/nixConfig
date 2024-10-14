1700b4d0b52c63298d64e4f25bb2f99f830f7bba
bnzuhair <relsalehzohair@gamil.com>
2024-10-14 05:54:06 +0300

    feat: oil delete to trash & no asking at semple tasks

--- modules/home-manager/nvim/config/oil.nix
+++ modules/home-manager/nvim/config/oil.nix
@@ -2,9 +2,11 @@
   config.plugins.oil = {
 		enable = true;
 		settings = {
-		default_file_explorer = true;
-		constrain_cursor = "editable";
-		useDefaultKeymaps = true;
+			default_file_explorer = true;
+			constrain_cursor = "editable";
+			useDefaultKeymaps = true;
+			delete_to_trash = true;
+			skip_confirm_for_simple_edits = true;
       };
   };
   config.keymaps = [
