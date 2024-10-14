c8479d66064f2eed46e98c5dfcce323d07b094b9
bnzuhair <relsalehzohair@gamil.com>
2024-10-14 22:15:24 +0300

    feat: show hedden files in oil

--- modules/home-manager/nvim/config/oil.nix
+++ modules/home-manager/nvim/config/oil.nix
@@ -7,6 +7,9 @@
 			useDefaultKeymaps = true;
 			delete_to_trash = true;
 			skip_confirm_for_simple_edits = true;
+			view_options = {
+				show_hidden = true;
+			};
       };
   };
   config.keymaps = [
