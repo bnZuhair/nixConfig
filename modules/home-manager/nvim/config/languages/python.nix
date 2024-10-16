1c9550cfb8b32d0474904200504f4a0b1c61ee90
bnzuhair <relsalehzohair@gamil.com>
2024-10-16 13:08:48 +0300

    feat: make the downloding of the formatters from the language files directly & do it for python

--- modules/home-manager/nvim/config/languages/python.nix
+++ modules/home-manager/nvim/config/languages/python.nix
@@ -1,21 +1,18 @@
 {
-	config.plugins.conform-nvim = {
-		settings = {
-			formatters_by_ft = {
-			  python = [ "black" ];
-			};
-		};
-	};
-	#config.plugins.lsp = {
-	#	servers.pyright = {
-	#		enable = true;
-	#		settings.plugins.black.enabled = true;
-   #   };
-   #};
-	config.plugins.lsp = {
-		servers.pylsp = {
-			enable = true;
-			settings.plugins.black.enabled = true;
-      };
-   };
+  config.plugins.conform-nvim = {
+    settings = { formatters_by_ft = { python = [ "black" ]; }; };
+  };
+  #config.plugins.lsp = {
+  #	servers.pyright = {
+  #		enable = true;
+  #		settings.plugins.black.enabled = true;
+  #   };
+  #};
+  config.plugins.lsp = {
+    servers.pylsp = {
+      enable = true;
+      settings.plugins.black.enabled = true;
+    };
+  };
+  config.plugins.none-ls.sources.formatting.black.enable = true;
 }
