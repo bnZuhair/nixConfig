cfb94778026229a528ee9c5203c78614a3b516ad
bnzuhair <relsalehzohair@gamil.com>
2024-10-17 14:17:41 +0300

    feat: make the nivm line better

--- modules/home-manager/nvim/config/keymaps.nix
+++ modules/home-manager/nvim/config/keymaps.nix
@@ -1,26 +1,26 @@
 {
-	config.keymaps = [
-		{
-			mode = "n";
-			action = "<cmd>nohlsearch<CR>";
-      	key = "<Esc>";
-			options = {
-      	  desc = "to Open Oil.nvim ";
-      	};
-	    }
-	{
-		mode = "n";
+  config.keymaps = [
+    {
+      mode = "n";
+      action = "<cmd>nohlsearch<CR>";
+      key = "<Esc>";
+      options = {
+        desc = "to Open Oil.nvim ";
+      };
+    }
+    {
+      mode = "n";
       action = "o<Esc>k";
       key = "<leader>o";
-		options = {
+      options = {
         desc = "make a new line down from normal mode and still be in it";
       };
     }
-	{
-		mode = "n";
+    {
+      mode = "n";
       action = "O<Esc>j";
       key = "<leader>O";
-		options = {
+      options = {
         desc = "a new line up from normal mode and still be in it";
       };
     }
--- modules/home-manager/nvim/config/ui.nix
+++ modules/home-manager/nvim/config/ui.nix
@@ -1,11 +1,93 @@
 {
-	config.colorschemes.tokyonight.enable = true;
-	config.plugins = {
-		lualine.enable = true;
-		web-devicons.enable = true;
-		indent-blankline = {
-			enable = true;
-			settings.indent.char = "│";
-		};
+  config.colorschemes.tokyonight.enable = true;
+  config.plugins = {
+    lualine = {
+      enable = true;
+      settings = {
+        options = {
+          globalstatus = true;
+          disabled_filetypes = {
+            statusline = [
+              "dashboard"
+              "alpha"
+              "starter"
+            ];
+          };
+        };
+        inactive_sections = {
+          lualine_x = [
+            "filename"
+            "filetype"
+          ];
+        };
+        sections = {
+          lualine_a = [
+            {
+              __unkeyed = "mode";
+              fmt = "string.lower";
+              separator.left = "";
+              separator.right = "";
+            }
+          ];
+          lualine_b = [
+            {
+              __unkeyed = "branch";
+              icon.__unkeyed = "";
+              separator.left = "";
+              separator.right = "";
+            }
+            {
+              __unkeyed = "diff";
+              separator.left = "";
+              separator.right = "";
+            }
+          ];
+          lualine_c = [
+            {
+              __unkeyed = "diagnostic";
+              symbols = {
+                error = " ";
+                warn = " ";
+                info = " ";
+                hint = "󰝶 ";
+              };
+              separator.left = "";
+              separator.right = "";
+            }
+          ];
+          lualine_x = [ "" ];
+          lualine_y = [
+            {
+              __unkeyed = "filetype";
+              icon_only = true;
+              separator.left = "";
+              separator.right = "";
+            }
+            {
+              __unkeyed = "filename";
+              symbols = {
+                modified = "";
+                readonly = "👁️";
+                unnamed = "";
+              };
+              separator.left = "";
+              separator.right = "";
+            }
+          ];
+          lualine_z = [
+            {
+              __unkeyed = "location";
+              separator.left = "";
+              separator.right = "";
+            }
+          ];
+        };
+      };
+    };
+    web-devicons.enable = true;
+    indent-blankline = {
+      enable = true;
+      settings.indent.char = "│";
+    };
   };
 }
