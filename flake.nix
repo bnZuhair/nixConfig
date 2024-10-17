fcd2e0cdba10e454554639b22a2ceaf996bafc1d
bnzuhair <relsalehzohair@gamil.com>
2024-10-17 21:48:03 +0300

    feat: enable unfree softwaref

--- flake.nix
+++ flake.nix
@@ -5,29 +5,40 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
     home-manager = {
-       url = "github:nix-community/home-manager/release-24.05";
-       inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }:
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-    nixosConfigurations = {
-      nixos = lib.nixosSystem {
-      inherit system;
-        modules = [ ./configuration.nix ];
+      #pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
       };
-    };
-    homeConfigurations = {
-      bnzuhair = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [ ./home.nix ];
+    in
+    {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+        };
       };
+      homeConfigurations = {
+        bnzuhair = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
+
     };
-    
-   };
 }
